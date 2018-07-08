b7f8fe4edb724974d56ede8dd1896b47ffd914e6
nagasitaramthigulla <nagasitaram.ram@gmail.com>
2018-07-08 07:56:16 +0530

    commit

--- qna/admin.py
+++ qna/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(UserProfile)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuestionVote)
+admin.site.register(AnswerVote)
+admin.site.register(CommentQuestion)
+admin.site.register(CommentAnswer)
