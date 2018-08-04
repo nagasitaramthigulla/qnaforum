d8a0fdb4a81627a2d2496e16c00e44b388e2ad49
nagasitaramthigulla <nagasitaram.ram@gmail.com>
2018-08-04 21:22:29 +0530

    commit 21:22:29.07,04-08-2018

---
+++ run.bat
@@ -0,0 +1,22 @@
+@echo off
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+IF %argC% == 0 (
+    GOTO FLAG
+)
+IF %1 == prod (
+    python manage.py runserver 0:80
+)ELSE IF %1 == dev (
+    python manage.py runserver 0:80 --settings qnaproject.settings
+)ELSE IF %1 == cs (
+    python manage.py collectstatic
+)ELSE (
+    :FLAG
+    @ECHO usage:
+    @ECHO. %0 COMMAND 
+    @ECHO.
+    @ECHO COMMAND options are:
+    @ECHO %TAB%       dev  development server
+    @ECHO %TAB%       prod  production server
+    @ECHO %TAB%       cs  collect static files
+)
